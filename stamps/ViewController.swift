f79d0c0d1aea867216219ca7cc1d4ca11c418f4d
kota.kaneko <kota.kaneko@livesense.co.jp>
2017-10-10 20:28:31 +0900

    Initial Commit

---
+++ stamps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stamps
+//
+//  Created by Spyc on H29/10/10.
+//  Copyright © 平成29年 Spyc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
